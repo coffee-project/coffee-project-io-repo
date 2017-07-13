b062241f5a899034715a8abf48e47285efa97956
rafszul <rafszul@gmail.com>
2017-07-13 06:10:46 +0100

    angular-mdl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdlModule } from '@angular-mdl/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MdlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
